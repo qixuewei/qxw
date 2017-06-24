d5c197b6f139b6b57f3b384d061cdfd8fd2f4316
qixuewei <xuewei930214@163.com>
2017-06-24 23:40:42 +0800

    提交了

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void go1(View view){
+            Toast.makeText(this, "点到了go1", Toast.LENGTH_SHORT).show();
+        }
+
+        public void go2(View view){
+            Toast.makeText(this, "点到了go2", Toast.LENGTH_SHORT).show();
+        }
+
+        public void go3(View view){
+            Toast.makeText(this, "点到了go3", Toast.LENGTH_SHORT).show();
+        }
+
+        public void go4(View view){
+            Toast.makeText(this, "点到了go4", Toast.LENGTH_SHORT).show();
+        }
+}
